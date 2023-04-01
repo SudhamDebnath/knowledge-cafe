439af97cfe8da80cef5450cf1f434e710b04bd7b
SudhamDebnath <sudham15-12854@diu.edu.bd>
2023-04-01 21:08:22 +0600

    Update user image

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,11 +8,11 @@ const Header = () => {
           <a className="navbar-brand fs-3 fw-bold">Knowledge Cafe</a>
           <div className="d-flex" role="search">
             <img
-              src="../../../public/girl.svg"
+              src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80"
               alt="Logo"
               width="50"
               height="50"
-              className="d-inline-block align-text-top"
+              className="d-inline-block align-text-top rounded-circle"
             />
           </div>
         </div>
